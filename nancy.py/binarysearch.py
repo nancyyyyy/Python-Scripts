8ad94a286d6a9754dc8b289ee651aeb7a72ae7ff
Nancy <somnancy123@gmail.com>
2020-08-01 21:17:57 +0530

    som is added

--- nancy.py/binarysearch.py
+++ nancy.py/binarysearch.py
@@ -10,6 +10,7 @@ def binary_tree(a,key,start,end):
             return mid
             # nancysom
             # nancy
+            #som
     print(key,"key could not found")
     return -1
 print(binary_tree(a,4,0,len(a)-1))
